e8581ad7628d8ed41eb8c29a48e51288a9e74713
lazy <jesseoaikhiena1@gmail.com>
2026-04-04 17:31:04 +0100

    Add simple expense calculator in JavaScript

---
+++ Day 1/simple expense calculator (day 1).js
@@ -0,0 +1,18 @@
+//Day one of learning Javascript
+//Variables an Arithemtic Operators
+
+let myBalance = 10000;
+//prices of items bought
+
+let banana = 500;
+let apple = 1000;
+let grape = 1500;
+
+//Calculate total cost of items bought
+let fruitPrices = banana + apple + grape;
+
+console.log("total cost of items bought: $" + fruitPrices)
+//calculate remaining balance
+
+let remainingBalance = myBalance - fruitPrices;
+console.log("remaining balance: $" + remainingBalance)
